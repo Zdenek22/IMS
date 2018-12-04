bf0e7bbf4157d30dcff06d1d84c2cc7791459d94
AdamGregor <spectroa@gmail.com>
2018-12-05 00:19:23 +0100

    Update rand.cpp

--- src/rand.cpp
+++ src/rand.cpp
@@ -2,9 +2,9 @@
 
 #define UINT_MAX   4294967295U
 float getRand() {
-	static unsigned ix = 598741262; 
-		ix = ix * 69069U + 1;
-		return ix / ((double)UINT_MAX + 1.0);
+	static unsigned ix = rand() % 4294967295 + 1;
+	ix = ix * 69069U + 1;
+	return ix / ((double)UINT_MAX + 1.0);
 }
 
 float getRandInRange(float low, float high) {
