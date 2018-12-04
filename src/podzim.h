de576a86e6d783d7085b500b44e23852ac67e509
AdamGregor <spectroa@gmail.com>
2018-12-05 00:05:29 +0100

    Add files via upload

---
+++ src/podzim.h
@@ -0,0 +1,20 @@
+#ifndef PODZIM
+#define PODZIM
+
+#define TERIDOX_VAL			1126.0
+#define COMMAND_36_VAL		4385.0
+#define DECIS_MEGA_VAL		1717.0
+#define KARATE_ZENON_VAL		2341.0
+#define STUTOX_VAL			181.5
+#define CCC_VAL			121.0
+#define CARAMBA_VAL			1057.4
+#define NURELLE_VAL			1020.0
+
+void aplikaceHerbicidu(float* naklady, float pole);
+void krytonosecSesulovy(float* naklady, float pole);
+void drepcik(float* naklady, float pole);
+void hrabosi(float* naklady, float pole);
+void regRustu(float* naklady, float pole);
+void pilatka(float* naklady, float pole);
+
+#endif
