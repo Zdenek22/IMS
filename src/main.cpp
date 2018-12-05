49ea24dd7b891e6e8f288ec16e69c1cccbada0a3
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-12-05 20:08:16 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -14,20 +14,28 @@ int main(int argc, char** argv){
 	float tmp;
 	float pole = std::stof(argv[1]);
 	int technologie = std::atoi(argv[2]);
+	float hnojivoCelkem = 0.0;
+	float postrikyCelkem = 0.0;
+	float regulatoryCelkem = 0.0;
+	float herbicidyCelkem = 0.0;
+	float pripravaCelkem = 0.0;
+	
 
 	pripravaPudy priprava;
-
-	std::cout<<"##############################################"<<std::endl << "##" << std::endl << "##" << std::endl;
+	
+	std::cout<<"\n\n############### PRIPRAVA PUDY ##################"<<std::endl << "##" << std::endl << "##" << std::endl;
 
 	tmp = naklady;
 	priprava.uklid(&naklady, pole);
 	std::cout << "##\tUklid slamy\t\tcelkove naklady" <<std::endl;
 	std::cout << "##\t"<<(naklady - tmp)<< " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	pripravaCelkem = pripravaCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	priprava.aplikaceHnojiv(&naklady, pole);
 	std::cout << "##\tAplikace hnojiv"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	hnojivoCelkem = hnojivoCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	// vyber technologie, 1 - klasicka, 2 - minimalizacni, 3 - bezorebna
@@ -35,59 +43,68 @@ int main(int argc, char** argv){
 		priprava.klasickaPriprava(&naklady, pole);
 		std::cout << "##\tKlasicke pripraveni pudy"<<std::endl;
 		std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+		pripravaCelkem = pripravaCelkem + (naklady - tmp);
 	}
 	else if(technologie == 2){
 		priprava.minimalizacniPriprava(&naklady, pole);
 		std::cout << "##\tMinimalizacni pripraveni pudy"<<std::endl;
 		std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+		pripravaCelkem = pripravaCelkem + (naklady - tmp);
 	}
 	else{
 		std::cout << "##\tBezorebne pripraveni pudy"<<std::endl;
 		std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+		pripravaCelkem = pripravaCelkem + (naklady - tmp);
 	}
 
 	tmp = naklady;
 	priprava.seti(&naklady, pole);
 	std::cout << "##\tVysazeni semen repky"<<std::endl;
-	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	pripravaCelkem = pripravaCelkem + (naklady - tmp);	
 
-	std::cout<<"##############################################\n##"<<std::endl;
 
+	std::cout<<"################### PODZIM #####################\n##"<<std::endl;
 
 	// PODZMIMNI OSETROVANI
 	tmp = naklady;
 	aplikaceHerbicidu(&naklady, pole);
 	std::cout << "##\tAplikace herbicidu"<<std::endl;
-	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	herbicidyCelkem = herbicidyCelkem + (naklady - tmp);	
 
 	tmp = naklady;
 	krytonosecSesulovy(&naklady, pole);
 	std::cout << "##\tPostrik proti krytonosci sesulovemu"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	drepcik(&naklady, pole);
 	std::cout << "##\tPostrik proti drepcikovi"<<std::endl;
-	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);	
 
 	tmp = naklady;
 	hrabosi(&naklady, pole);
 	std::cout << "##\tPostrik proti hrabosum"<<std::endl;
-	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);	
 
 	tmp = naklady;
 	regRustu(&naklady, pole);
 	std::cout << "##\tAplikace regulatoru vzrustu"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	regulatoryCelkem = regulatoryCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	pilatka(&naklady, pole);
 	std::cout << "##\tPostrik proti pilatkam"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);
 
 
-
-	std::cout<<"##############################################\n##"<<std::endl;
+	std::cout<<"################### JARO #######################\n##"<<std::endl;
 
 	// JARNI OSETROVANI
 	jaro jarniOsetreni;
@@ -95,52 +112,78 @@ int main(int argc, char** argv){
 	tmp = naklady;
 	jarniOsetreni.regenHnojeni(&naklady, pole);
 	std::cout << "##\tRegeneracni hnojeni"<<std::endl;
-	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	hnojivoCelkem = hnojivoCelkem + (naklady - tmp);	
 
 	tmp = naklady;
 	jarniOsetreni.produkHnojeni(&naklady, pole);
 	std::cout << "##\tProdukcni hnojeni"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	hnojivoCelkem = hnojivoCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	jarniOsetreni.krytonosecRepkovy(&naklady, pole);
 	std::cout << "##\tOchrana proti krytonosci repkovemu"<<std::endl;
-	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);	
 
 	tmp = naklady;
 	jarniOsetreni.regulatorRustu(&naklady, pole);
 	std::cout << "##\tAplikace regulatoru rustu"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	regulatoryCelkem = regulatoryCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	jarniOsetreni.blyskacek(&naklady, pole);
 	std::cout << "##\tOchrana proti blyskackovi"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	jarniOsetreni.listovaHnojiva(&naklady, pole);
 	std::cout << "##\tAplikace listovych hnojiv"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	hnojivoCelkem = hnojivoCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	jarniOsetreni.doladovaciDusik(&naklady, pole);
 	std::cout << "##\tDoladovaci davka dusiku"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	hnojivoCelkem = hnojivoCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	jarniOsetreni.bejlomorka(&naklady, pole);
 	std::cout << "##\tOchrana proti bejlomorce"<<std::endl;
-	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);	
 
 	tmp = naklady;
 	jarniOsetreni.krytonosecSesulovy(&naklady, pole);
 	std::cout << "##\tOchrana proti krytonosci sesulovemu"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);
 
 	tmp = naklady;
 	jarniOsetreni.msice(&naklady, pole);
 	std::cout << "##\tOchrana proti msicim"<<std::endl;
 	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+	postrikyCelkem = postrikyCelkem + (naklady - tmp);
+
+	tmp = naklady;
+	jarniOsetreni.sklizen(&naklady, pole);
+	std::cout << "##\tSklizen"<<std::endl;
+	std::cout << "##\t"<<(naklady -tmp) << " Kc\t\t"<< naklady<< " Kc"<<std::endl<<"##"<<std::endl;	
+
+	
+	std::cout<<"############## CELKOVE HODNOTY #################\n##"<<std::endl;
+	std::cout << "##\tPriprava + seti\t\t"<< pripravaCelkem << " Kc"<<std::endl;
+	std::cout << "##\tHnojivo\t\t\t"<< hnojivoCelkem << " Kc"<<std::endl;
+	std::cout << "##\tOchrana proti skudcum\t"<< postrikyCelkem << " Kc"<<std::endl;
+	std::cout << "##\tRegulatory\t\t"<< regulatoryCelkem << " Kc"<<std::endl;
+	std::cout << "##\tHerbicidy\t\t"<< herbicidyCelkem << " Kc"<<std::endl;
+	std::cout << "##\t________________________________________"<<std::endl;
+	std::cout << "##\tCena celken\t\t"<< naklady << " Kc"<<std::endl;
+
 
 	return 0;
 }
--- src/nafta.cpp
+++ src/nafta.cpp
@@ -5,11 +5,12 @@
 */
 
 #include "nafta.hpp"
+#include "rand.h"
 
 // spotreba nafty pri seti
 void nafta::naftaSeti(float* naklady, float pole){
 	// spotreba l/ha
-	float spotreba = 8.0;
+	float spotreba = getRandInRange(7.8, 8.3);
 
 	*naklady = *naklady + (spotreba * pole * getCenaNafty());
 }
@@ -18,13 +19,13 @@ void nafta::naftaSeti(float* naklady, float pole){
 void nafta::naftaHnojeni(float* naklady, float pole, int davka){
 	float spotreba;
 	if(davka <= 300)
-		spotreba = 1.9;
+		spotreba = getRandInRange(1.8, 2.2);
 
 	else if(davka > 300 && davka < 500)
-		spotreba = 2.4;
+		spotreba = getRandInRange(2.3, 2.5);
 
 	else
-		spotreba = 3.0;
+		spotreba = getRandInRange(2.6, 3.0);
 
 
 	*naklady = *naklady + (spotreba * pole * getCenaNafty());
@@ -32,42 +33,42 @@ void nafta::naftaHnojeni(float* naklady, float pole, int davka){
 
 // spotreba nafty pri postriku pole
 void nafta::naftaPostrik(float* naklady, float pole){
-	float spotreba = 2.5;
+	float spotreba = getRandInRange(2.5, 2.8);
 
 	*naklady = *naklady + (spotreba * pole * getCenaNafty());
 }	
 
 // spotreba nafty pri sberu slamy predplodiny
 void nafta::naftaSlama(float* naklady, float pole){
-	float spotreba = 1.3;
+	float spotreba = getRandInRange(1.3, 1.6);
 
 	*naklady = *naklady + (spotreba * pole * getCenaNafty());
 }
 
 // spotreba nafty pri sklizni repky
 void nafta::naftaSklizen(float* naklady, float pole){
-	float spotreba = 15.0;
+	float spotreba = getRandInRange(14.9, 15.5);
 
 	*naklady = *naklady + (spotreba * pole * getCenaNafty());
 }
 
 // spotreba nafty u podmitky radlicnym podmitacem
 void nafta::naftaRadlicny(float* naklady, float pole){
-	float spotreba = 11.5;
+	float spotreba = getRandInRange(11.3, 11.5);;
 
 	*naklady = *naklady + (spotreba * pole * getCenaNafty());
 }
 
 // spotreba nafty u podmimtky talirovym podmitacem	
 void nafta::naftaTalirovy(float* naklady, float pole){
-	float spotreba = 5.9;
+	float spotreba = getRandInRange(5.8, 6.1);
 
 	*naklady = *naklady + (spotreba * pole * getCenaNafty());
 }
 
 // spotreba nafty pri orbe
 void nafta::naftaOrba(float* naklady, float pole){
-	float spotreba = 18.0;
+	float spotreba = getRandInRange(18.1, 18.5);;
 
 	*naklady = *naklady + (spotreba * pole * getCenaNafty());
 }
